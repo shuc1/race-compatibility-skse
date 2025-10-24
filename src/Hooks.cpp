c376c9d1263a24fd558c36b75f514c7e2fb25992
shuc <70183094+shuc1@users.noreply.github.com>
2025-10-24 11:58:11 +0800

    fix: forward-declare FuncStorage before InstallHooks to resolve compilation error chore: maintenance

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -44,9 +44,20 @@ namespace
 	template <typename... Ts>
 	constexpr auto MakeHookMessage()
 	{
-		static constexpr auto msg_array = make_hook_message_array<Ts...>();
-		return std::string_view{ msg_array.data(), msg_array.size() - 1 };
+		static constexpr auto message = make_hook_message_array<Ts...>();
+		return std::string_view{ message.data(), message.size() - 1 };
 	}
+}
+
+namespace
+{
+#ifdef DETOURS
+	template <stl::Hookable T>
+	struct FuncStorage
+	{
+		static inline decltype(&T::Thunk) func{ nullptr };
+	};
+#endif
 
 	template <stl::Hookable T>
 	void InstallHook()
@@ -64,11 +75,11 @@ namespace
 		const REL::Relocation src{ T::id, offset };
 #ifdef DETOURS
 		FuncStorage<T>::func = reinterpret_cast<decltype(&T::Thunk)>(src.address());
-		DetourAttach(reinterpret_cast<PVOID*>(&FuncStorage<T>::func),
-			reinterpret_cast<PVOID>(T::Thunk));
+		DetourAttach(reinterpret_cast<PVOID*>(&FuncStorage<T>::func), reinterpret_cast<PVOID>(T::Thunk));
 #else
 		stl::write_jump_to_thunk<T>(src.address());
 #endif
+
 #ifndef NDEBUG
 		logs::info("Installed hook for {} at address: 0x{:X}",
 			std::string_view(stl::struct_name_for<T>.data(), stl::struct_name_for<T>.size() - 1),
@@ -97,14 +108,6 @@ namespace
 namespace
 {
 	using namespace rcs;
-
-#ifdef DETOURS
-	template <stl::Hookable T>
-	struct FuncStorage
-	{
-		static inline decltype(&T::Thunk) func{ nullptr };
-	};
-#endif
 
 	namespace
 	{
@@ -173,33 +176,34 @@ namespace
 		struct GetPCIsRace
 		{
 			static constexpr auto id = REL::ID(21484);
+
 #	ifndef DETOURS
-			// after loading player character into register
-			static constexpr std::ptrdiff_t offset = 0x7;
+#		pragma pack(push, 1)
+			// custom assembly to get PlayerCharacter*
+			struct Assembly
+			{
+				// mov r8,QWORD PTR [rip + (disp)] # address of PlayerCharacter*
+				// 4c 8b 05 c1 9f e5 02, for example in 1.6.1170
+				// rcx is unused, but the test shows that a value is still loaded into it.
+				std::uint8_t rex{ 0x4c };    // 0x0
+				std::uint8_t mov{ 0x8b };    // 0x1
+				std::uint8_t modrm{ 0x05 };  // 0x2, r8
+				std::int32_t disp;           // 0x3
+			};
+			static_assert(offsetof(Assembly, rex) == 0x0);
+			static_assert(offsetof(Assembly, mov) == 0x1);
+			static_assert(offsetof(Assembly, modrm) == 0x2);
+			static_assert(offsetof(Assembly, disp) == 0x3);
+			static_assert(sizeof(Assembly) == 0x7);
+#		pragma pack(pop)
+
+			static constexpr std::ptrdiff_t offset = sizeof(Assembly);  // after loading player character into register
 
 			static void PreInstall()
 			{
 				const auto src = REL::Relocation{ id, 0 }.address();
 				const auto pc = REL::Relocation{ RE::Offset::PlayerCharacter::Singleton, 0 }.address();
-#		pragma pack(push, 1)
-				struct Assembly
-				{
-					// mov r8,QWORD PTR [rip + (disp)] # address of PlayerCharacter*
-					// 4c 8b 05 c1 9f e5 02, for example in 1.6.1170
-					// rcx is marked [[maybe_unused]], but the test shows that a value is still loaded into it.
-					std::uint8_t rex{ 0x4c };    // 0x0
-					std::uint8_t mov{ 0x8b };    // 0x1
-					std::uint8_t modrm{ 0x05 };  // 0x2, r8
-					std::int32_t disp;           // 0x3
-				} const assembly{
-					.disp = static_cast<std::int32_t>(pc - (src + offset))
-				};
-				static_assert(offsetof(Assembly, rex) == 0x0);
-				static_assert(offsetof(Assembly, mov) == 0x1);
-				static_assert(offsetof(Assembly, modrm) == 0x2);
-				static_assert(offsetof(Assembly, disp) == 0x3);
-				static_assert(sizeof(Assembly) == offset);
-#		pragma pack(pop)
+				const auto assembly = Assembly{ .disp = static_cast<std::int32_t>(pc - (src + sizeof(Assembly))) };
 
 				REL::safe_write(src, &assembly, sizeof(assembly));
 			}
@@ -208,11 +212,12 @@ namespace
 			static bool Thunk([[maybe_unused]] const RE::TESObjectREFR* unused1, const RE::TESForm* race_form, [[maybe_unused]] void* unused2, double& result)
 			{
 				result = 0.0;
+
 #	ifdef DETOURS
 				const auto pc = RE::PlayerCharacter::GetSingleton();
 #	else
 				// used custom assembly to get PlayerCharacter*
-				const auto pc = reinterpret_cast<const RE::PlayerCharacter*>(unused2);
+				const auto pc = static_cast<const RE::PlayerCharacter*>(unused2);
 #	endif
 				if (pc && race_form) {
 					if (const auto race = race_form->As<RE::TESRace>();
