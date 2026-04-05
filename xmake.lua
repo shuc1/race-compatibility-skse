6687fd4d76d85fcb9bb3f5e9ac3677323ca44ac2
shuc <70183094+shuc1@users.noreply.github.com>
2026-04-05 19:06:30 +0800

    chore: enhance CI/CD workflows

--- xmake.lua
+++ xmake.lua
@@ -10,7 +10,7 @@ projectabbr = "RCS"
 papyrusname = "RaceCompatibility" -- for potential overridden by papyrus implementation
 
 set_project(projectname)
-set_version("2.5.2", {build = "%Y-%m-%d"})
+set_version("2.5.3", {build = "%Y-%m-%d"})
 set_license("GPL-3.0")
 
 set_policy("package.requires_lock", true)
